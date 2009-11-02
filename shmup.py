166a8de04e5e679b403615d226c9ef3740e05689
Bruno Dilly <bruno.dilly@brunodilly.org>
2009-11-02 18:02:48 -0200

    Add animated background based on tiles and change default resolution to 800x600

--- shmup.py
+++ shmup.py
@@ -14,6 +14,8 @@
 # imports module that provides access to some objects used or maintained
 # by the interpreter. Useful to get argv and path.
 import sys
+# operational system routines
+import os
 # imports parser for command line arguments in sys.argv
 import getopt
 # imports all the available pygame modules
@@ -22,8 +24,69 @@ import pygame
 # of our script
 from pygame.locals import *
 
+# sets resources directories
+data_dir = os.path.join(".","data")
+images_dir = os.path.join(data_dir, "graphic")
+audio_dir = os.path.join(data_dir, "audio")
+music_dir = os.path.join(data_dir, "music")
+
 # DEBUG enable debug output
 DEBUG = True
+
+
+class Background:
+    """
+    It's the animated game background created with tiles.
+    """
+    image = None
+    pos = None
+
+    def __init__(self, image):
+        """
+        Creates a surface with tiles repeated until cover the entire screen.
+        """
+        # sets the absolute path
+        image = os.path.join(images_dir, image)
+        # convert the image so it won't be done every blit
+        # it improves the performance
+        image = pygame.image.load(image).convert()
+        self.size = image.get_size()
+        self.pos = [0, 0]
+        screen = pygame.display.get_surface()
+        screen_size = screen.get_size()
+
+        from math import ceil
+        # ceil returns the ceiling of x as a float
+        # we want the size of the screen plus a tile
+        w = (ceil(float(screen_size[0]) / self.size[0]) + 1) * self.size[0]
+        h = (ceil(float(screen_size[1]) / self.size[1]) + 1) * self.size[1]
+
+        # creates a surface with width w and height h
+        back = pygame.Surface((w, h))
+
+        # covers the entire screen
+        for i in range((back.get_size()[0]/self.size[0])):
+            for j in range((back.get_size()[1]/self.size[1])):
+                back.blit(image, (i * self.size[0], j * self.size[1]))
+
+        self.image = back
+
+    def update(self, dt):
+        """
+        Moves the background to the left
+        """
+        self.pos[0] -= 1
+        # when it reaches the end, moves the background for the start point
+        if (self.pos[0] < -self.size[0]):
+            self.pos[0] += self.size[0]
+        if DEBUG:
+            print "BACKGROUND POSITION: " + str(self.pos[0])
+
+    def draw(self,screen):
+        """
+        Draws the background
+        """
+        screen.blit(self.image, self.pos)
 
 
 class Game:
@@ -63,10 +126,19 @@ class Game:
             elif type == KEYDOWN and key == K_ESCAPE:
                 self.run = False
 
+    def actors_update(self, dt):
+        self.background.update(dt)
+
+    def actors_draw(self):
+        self.background.draw(self.screen)
+
     def loop(self):
         """
         Main loop
         """
+        # creates the background
+        self.background = Background("tile.png")
+
         #starts clock
         clock = pygame.time.Clock()
         dt = 16
@@ -74,8 +146,15 @@ class Game:
         while self.run:
             clock.tick(1000/dt)
 
+            # handle input
             self.handle_events()
+            # update all the game elements
+            self.actors_update(dt)
+            # draw the elements to the back buffer
+            self.actors_draw()
+            # flip the front and back buffer
             pygame.display.flip()
+
             if DEBUG:
                 print "FPS: %0.2f" % clock.get_fps()
 
@@ -110,7 +189,7 @@ def parse_opts(argv):
         sys.exit(2)
 
     # default options
-    options = {"fullscreen": False, "resolution": (640,480)}
+    options = {"fullscreen": False, "resolution": (800,600)}
 
     for o, a in opts:
         if o in ("-f", "--fullscreen"):
@@ -132,6 +211,11 @@ def main(argv):
     """
     Gets command line options and starts the game
     """
+    # change to the correct directory to find graphical resources and others
+    abspath = os.path.abspath(argv[0])
+    dir = os.path.dirname(abspath)
+    os.chdir(dir)
+
     options = parse_opts(argv)
     game = Game(options["resolution"], options["fullscreen"])
     # starts game's main loop
