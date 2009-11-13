be23c97bd3be6547f7fcd1da9851ff2db9e7e273
João <jcorrea@marvim.(none)>
2009-11-12 23:08:21 -0200

    Fixed a bug in element's list ordering with some ninja black magic skillz Slightly prototyped how manage_elements() will deal with different types of elements.

---
+++ code/stage.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 (i think) -*-
+
+#----------------------------------------------------------------------
+# Author:
+#   João Corrêa <joao@livewire.com.br>
+#
+# Copyright (C) 2009 João Corrêa
+#
+# Released under GNU GPL, read the file 'COPYING' for more information
+# ----------------------------------------------------------------------
+
+import xml
+import xml.dom
+import xml.dom.minidom
+
+class Callable:
+    
+    def __init__(self, anycallable):
+        self.__call__ = anycallable
+
+class Item:
+    def __init__(self, x, y, rot, rotspeed, life, image, type, behaviour, special):
+        self.x = x
+        self.y = y
+        self.rot = rot
+        self.rotspeed = rotspeed
+        self.life = life
+        self.image = image
+        self.type = type
+        self.behaviour = behaviour
+        self.special = special
+
+class Stage:
+
+    L = list()
+
+    def __init__(self, file):
+        self.data = xml.dom.minidom.parse(file)
+
+    def __cmp__(self, other):
+	return cmp(self.x, other.x)
+
+    def getNextX(self):
+	if len(self.L) > 0:
+	    item = self.L[len(self.L) - 1]
+	    return item.x
+	else:
+	    return -1
+
+    def include(self, item):
+        self.L.append(item)
+
+    def pop(self, position):
+        subList = list()
+        nextx = self.getNextX()
+        while nextx == position:
+            subList.append(self.L.pop())
+            nextx = self.getNextX()   
+        return subList
+
+    def buildStage(self):
+    	for nodes in self.data.getElementsByTagName("item"):
+            x = int(nodes.getElementsByTagName("x")[0].childNodes[0].nodeValue)
+ 	    y = int(nodes.getElementsByTagName("y")[0].childNodes[0].nodeValue)
+            rot = int(nodes.getElementsByTagName("rot")[0].childNodes[0].nodeValue)
+            rotspeed = int(nodes.getElementsByTagName("rotspeed")[0].childNodes[0].nodeValue)
+            life = int(nodes.getElementsByTagName("life")[0].childNodes[0].nodeValue)
+            image = nodes.getElementsByTagName("image")[0].childNodes[0].nodeValue
+            itemType = nodes.getElementsByTagName("type")[0].childNodes[0].nodeValue
+            behaviour = nodes.getElementsByTagName("behaviour")[0].childNodes[0].nodeValue
+            special = nodes.getElementsByTagName("special")[0].childNodes[0].nodeValue
+            item = Item(x, y, rot, rotspeed, life, image, itemType, behaviour, special)
+            self.include(item)
+	    #print "New object on xml file:"
+            #print x
+	    #print y
+            #print life
+	    #print image
+            #print itemtype
+	    #print behaviour
+            #print special
+	#below is some black magic I'm not sure how to deal with... but works
+	#taken from stackoverflow.org
+	self.L.sort(key=lambda item: item.x, reverse=True)
